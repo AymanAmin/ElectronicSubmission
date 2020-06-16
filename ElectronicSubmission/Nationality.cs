e418689d5a86c512e7a5ae25abe5469bc5b66850
Ayman Amin <Ayman1793@hotmail.com>
2020-06-16 15:39:09 +0300

    Save Change

--- ElectronicSubmission/Nationality.cs
+++ ElectronicSubmission/Nationality.cs
@@ -19,14 +19,12 @@ namespace ElectronicSubmission
             this.Students = new HashSet<Student>();
         }
     
-
         public int Nationality_Id { get; set; }
         public string Country_code { get; set; }
         public string Country_Name_En { get; set; }
         public string Country_Name_Ar { get; set; }
         public string Nationality_Name_En { get; set; }
         public string Nationality_Name_Ar { get; set; }
-        public int Nationality_Id { get; set; }
     
         public virtual Nationality Nationality1 { get; set; }
         public virtual Nationality Nationality2 { get; set; }
